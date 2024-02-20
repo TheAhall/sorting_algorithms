c6e7832b68add84cbd63095f459ed5886ae5216e
Hallal <hmdhallal@gmail.com>
2024-02-20 20:24:21 +0000

    0x1B. C - Sorting algorithms & Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,47 @@
 #include "sort.h"
-
 /**
- * node_swap - swaps a node of a list
- * @node: the node
- * @list: the list
- * Return: the final list
+ * insertion_sort_list - sort a list
+ * @list: UnSorted Linked list
  */
-listint_t *node_swap(listint_t *node, listint_t **list)
-{
-	listint_t *prev = node->prev, *curr = node;
 
-	prev->next = curr->next;
-	if (curr->next != NULL)
-		curr->next->prev = prev;
-	curr->next = prev;
-	curr->prev = prev->prev;
-	prev->prev = curr;
-	if (curr->prev != NULL)
-		curr->prev->next = curr;
-	else
-		*list = curr;
-	return (curr);
-}
-
-/**
- * insertion_sort_list - the Insertion sort algorithm
- * @list: the list should be sorted
- * Return: void
- */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *node;
 
 	if (list == NULL || (*list)->next == NULL)
 		return;
-	while (node != NULL)
+	node = (*list)->next;
+	while (node)
 	{
-		while(node->prev != NULL && node->prev->n > node->n)
+		while ((node->prev) && (node->prev->n > node->n))
 		{
-			node = node_swap(node, list);
+			node = swap_node(node, list);
 			print_list(*list);
 		}
 		node = node->next;
 	}
+}
+/**
+ *swap_node - swap a node helper
+ *@node: node
+ *@list: node list
+ *Return: return pointer of current node
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
