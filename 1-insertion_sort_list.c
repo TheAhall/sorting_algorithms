f0934389a9262f86b7b14a465de7571254cfe8f9
HALLAL <hmdhallal@gmail.com>
2024-02-20 08:25:09 -0800

    0x1B. C - Sorting algorithms & Big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - the Insertion sort algorithm
+ * @list: the list should be sorted
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i, j, tmp;
+
+	if (list == NULL)
+		return;
+}
